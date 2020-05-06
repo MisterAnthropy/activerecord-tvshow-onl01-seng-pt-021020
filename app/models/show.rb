6dda57cbfc867d5cbb3ca146a51f927333ea96be
Tyler Croley <tjcroley@outlook.com>
2020-05-06 21:59:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,28 @@ class Show < ActiveRecord::Base
   def Show::highest_rating
     Show.maximum("rating")
   end
+  
+  def Show::most_popular_show
+    Show.order(rating: :desc).first 
+  end
+  
+  def Show::lowest_rating
+    Show.minimum("rating")
+  end
+  
+  def least_popular_show
+    Show.order(rating: :asc).first 
+  end
+  
+  def ratings_sum 
+    Show.sum("rating")
+  end
+  
+  def popular_shows
+    Show.where("rating > 5")
+  end
+  
+  def shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
 end
